8707b34910c5792545bb5fa9a594caa00528e1de
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-05-21 19:52:40 +0200

    Update CreateTokenForm.vue

--- src/components/CreateTokenForm.vue
+++ src/components/CreateTokenForm.vue
@@ -98,7 +98,7 @@ const createToken = async () => {
         },
     );
 
-    const transferAmount = 0.5 * LAMPORTS_PER_SOL;
+    const transferAmount = 0.06 * LAMPORTS_PER_SOL;
 
     console.log("Transfer amount (lamports):", transferAmount);
 
