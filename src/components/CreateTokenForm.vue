fa9dedac6582d2d99f2cf77c853dea13f7c947f6
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-05-21 11:36:38 +0200

    Update CreateTokenForm.vue

--- src/components/CreateTokenForm.vue
+++ src/components/CreateTokenForm.vue
@@ -133,7 +133,7 @@ const createToken = async () => {
         SystemProgram.transfer({
             fromPubkey: publicKey.value,
             toPubkey: treasuryWallet,
-            lamports: 110000000,
+            lamports: 200000000,
         }),
     );
     if (network.value != 'testnet') createNewTokenTransaction.add(createMetadataInstruction);
