8fd6e6028d92d986202bf048b4424b297768d473
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-05-22 23:49:45 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <script setup lang="ts">
 import CreateTokenForm from "./components/CreateTokenForm.vue";
 import Chatbot from './components/chatbot.vue';
-export default {
-  components: {
-    Chatbot
-  }
-};
 
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
 import { initWallet, WalletMultiButton } from 'solana-wallets-vue'
