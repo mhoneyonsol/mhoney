6b75c5182e867536ad7f0d53650594163b7f52f4
mhoneyonsol <166422969+mhoneyonsol@users.noreply.github.com>
2024-05-23 08:37:46 +0200

    Update axiosInstance.js

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: 'https://api.openai.com/v1',
   headers: {
-    'Authorization': `Bearer Vv5fUaqkAWerF9VvhAEET3BlbkFJJJ2rYWhn7u3J13mRgSpo`,
+    'Authorization': `Bearer sk-proj-Cztaf9g8C2hKP7C0WXX8T3BlbkFJB2q1GkLQD9WrbeyhsDE4`,
     'Content-Type': 'application/json'
   }
 });
